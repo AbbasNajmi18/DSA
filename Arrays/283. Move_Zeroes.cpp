125717519f1dbe8728f7170e626a19bdc1a7c341
AbbasNajmi18 <95100974+AbbasNajmi18@users.noreply.github.com>
2023-07-12 22:31:23 +0530

    Create 283. Move_Zeroes.cpp

---
+++ Arrays/283. Move_Zeroes.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/move-zeroes/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int end=nums.size();
+        int zero=0;
+        int start=0;
+        while(start<end){
+            if(nums[start]!=0){
+                swap(nums[start],nums[zero]);
+                zero++;
+            }
+            start++;
+        }
+    }
+};
